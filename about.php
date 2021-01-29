a2aeaca98b8ba5e25a5c5a7fe617729227fa6b01
Massimo-stalnoy-strelok-iz-myasa <57673259+Massimo-stalnoy-strelok-iz-myasa@users.noreply.github.com>
2021-01-29 12:03:57 +0300

    ya
    yayay

---
+++ about.php
@@ -0,0 +1,68 @@
+﻿<!DOCTYPE HTML>
+<html lang="ru">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0>
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+<link rel="stylesheet" href="style.css">
+<title>Контактная форма</title>
+</head>
+<body>
+<div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom shadow-sm">
+  <h5 class="my-0 mr-md-auto font-weight-normal">Maxibon</h5>
+  <nav class="my-2 my-md-0 mr-md-3">
+    <a class="p-2 text-dark" href="index.php">Главная</a>
+    <a class="p-2 text-dark" href="about.php">Контакты</a>
+  </nav>
+  <a class="btn btn-outline-primary" href="#">Войти</a>
+</div>
+<div class="container mt-5" >
+<h3>Контактная форма</h3>
+<input type="enail" name="email" placeholder="Введите ваш Email"
+class="form-control"><br>
+<textarea name="message" class="form-control"
+placeholder="Введите ваше сообщение"></textarea><br>
+<button type="submit" name"send" class="btn btn-success">Отправить</button>
+</div>
+<footer class="container pt-4 my-md-5 pt-md-100 border-top">
+    <div class="row">
+      <div class="col-16 col-md">
+        <img class="mb-2" src="plosk.jpg" alt="" width="200" height="100">
+        <small class="d-block mb-3 text-muted">© 2017-2021</small>
+      </div>
+      <div class="col-6 col-md">
+        <h5>Features</h5>
+        <ul class="list-unstyled text-small">
+          <li><a class="link-secondary" href="#">Cool stuff</a></li>
+          <li><a class="link-secondary" href="#">Random feature</a></li>
+          <li><a class="link-secondary" href="#">Team feature</a></li>
+          <li><a class="link-secondary" href="#">Stuff for developers</a></li>
+          <li><a class="link-secondary" href="#">Another one</a></li>
+          <li><a class="link-secondary" href="#">Last time</a></li>
+        </ul>
+      </div>
+      <div class="col-6 col-md">
+        <h5>Resources</h5>
+        <ul class="list-unstyled text-small">
+          <li><a class="link-secondary" href="#">Resource</a></li>
+          <li><a class="link-secondary" href="#">Resource name</a></li>
+          <li><a class="link-secondary" href="#">Another resource</a></li>
+          <li><a class="link-secondary" href="#">Final resource</a></li>
+        </ul>
+      </div>
+      <div class="col-6 col-md">
+        <h5>About</h5>
+        <ul class="list-unstyled text-small">
+          <li><a class="link-secondary" href="#">Team</a></li>
+          <li><a class="link-secondary" href="#">Locations</a></li>
+          <li><a class="link-secondary" href="#">Privacy</a></li>
+          <li><a class="link-secondary" href="#">Terms</a></li>
+        </ul>
+      </div>
+    </div>
+  </footer>
+
+</div>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,103 @@
+﻿<!DOCTYPE HTML>
+<html lang="ru">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0>
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+<link rel="stylesheet" href="style.css">
+<title>Сайт Общества Плоской Земли</title>
+</head>
+<body>
+<div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom shadow-sm">
+  <h5 class="my-0 mr-md-auto font-weight-normal">Maxibon</h5>
+  <nav class="my-2 my-md-0 mr-md-3">
+    <a class="p-2 text-dark" href="index.php">Главная</a>
+    <a class="p-2 text-dark" href="about.php">Контакты</a>
+  </nav>
+  <a class="btn btn-outline-primary" href="#">Войти</a>
+</div>
+<img src="plosk.jpg" alt="">
+<div class="container mt-5">
+<h3 class="mb-5"> Информация </h3>
+<div class="d-flex flex-wrap">
+<div class="card mb-4 shadow-sm">
+      <div class="card-header">
+        <h4 class="my-2 fw-normal">Факты!</h4>
+      </div>
+      <div class="card-body">
+        <ul class="list-unstyled mt-3 mb-4">
+          <li>
+			  Чикаго видно с расстояния 140 километров, но это невозможно, учитывая шарообразность планеты!
+			  Согласно «шарообразному» строению планеты, на большой высоте пилоты самолетов должны были бы опускать нос транспортного средства спустя каждые 5 минут, однако этого не происходит!
+			  NASA не представляло видеотрансляцию со спутников или шаттлов в прямом эфире, хотя это вполне осуществимо!
+			  Почему по сей день в Антарктиду запрещен доступ простым исследователям!
+			  Если бы купол не состоял из кислорода, экологической проблемы не существовало бы!</li>
+        </ul>
+      </div>
+    </div>
+	<div class="card mb-4 shadow-sm">
+      <div class="card-header">
+        <h4 class="my-2 fw-normal">Чуть-чуть о нас</h4>
+      </div>
+      <div class="card-body">
+        <ul class="list-unstyled mt-3 mb-4">
+          <li>Общество плоской Земли (англ. Flat Earth Society) — организация, основанная в Англии и позднее возрождённая в США, пропагандирующая идею плоской Земли. Общество плоской Земли отстаивало идею, согласно которой Земля имеет форму плоского диска.</li>
+        </ul>
+      </div>
+    </div>
+	<div class="card mb-4 shadow-sm">
+      <div class="card-header">
+        <h4 class="my-2 fw-normal">Тезисы</h4>
+      </div>
+      <div class="card-body">
+        <ul class="list-unstyled mt-3 mb-4">
+          <li>Земля является плоским диском 40 000 километров в диаметре, с центром в районе Северного полюса.
+Солнце и Луна вращаются над поверхностью Земли. То же самое происходит со звёздами.
+Гравитация отрицается. Ускорение свободного падения возникает ввиду того, что Земля движется вверх с ускорением 9,8 м/с². Благодаря искривлению пространства-времени так можно ускоряться бесконечно долго.
+Южного полюса не существует. То, что нам кажется Антарктикой — ледяная стена, опоясывающая мир.
+Все фотографии Земли из космоса — подделки.</li>
+        </ul>
+      </div>
+    </div>
+<footer class="container pt-4 my-md-5 pt-md-100 border-top">
+    <div class="row">
+      <div class="col-16 col-md">
+        <img class="mb-2" src="plosk.jpg" alt="" width="200" height="100">
+        <small class="d-block mb-3 text-muted">© 2017-2021</small>
+      </div>
+      <div class="col-6 col-md">
+        <h5>Features</h5>
+        <ul class="list-unstyled text-small">
+          <li><a class="link-secondary" href="#">Cool stuff</a></li>
+          <li><a class="link-secondary" href="#">Random feature</a></li>
+          <li><a class="link-secondary" href="#">Team feature</a></li>
+          <li><a class="link-secondary" href="#">Stuff for developers</a></li>
+          <li><a class="link-secondary" href="#">Another one</a></li>
+          <li><a class="link-secondary" href="#">Last time</a></li>
+        </ul>
+      </div>
+      <div class="col-6 col-md">
+        <h5>Resources</h5>
+        <ul class="list-unstyled text-small">
+          <li><a class="link-secondary" href="#">Resource</a></li>
+          <li><a class="link-secondary" href="#">Resource name</a></li>
+          <li><a class="link-secondary" href="#">Another resource</a></li>
+          <li><a class="link-secondary" href="#">Final resource</a></li>
+        </ul>
+      </div>
+      <div class="col-6 col-md">
+        <h5>About</h5>
+        <ul class="list-unstyled text-small">
+          <li><a class="link-secondary" href="#">Team</a></li>
+          <li><a class="link-secondary" href="#">Locations</a></li>
+          <li><a class="link-secondary" href="#">Privacy</a></li>
+          <li><a class="link-secondary" href="#">Terms</a></li>
+        </ul>
+      </div>
+    </div>
+  </footer>
+
+</div>
+</body>
+</html>
